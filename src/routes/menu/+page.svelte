31eb5a11bc27c39a888b5f43832075b2db3a1c28
Giacomo Nanni <gn.nanni@gmail.com>
2025-10-30 12:44:12 +0100

    feat menu

---
+++ src/routes/menu/+page.svelte
@@ -0,0 +1,14 @@
+<a data-sveltekit-reload href="/">Home</a> <br /><br />
+<a data-sveltekit-reload href="/latest">Latest</a> <br /><br />
+<a data-sveltekit-reload href="/timeline">Timeline</a> <br /><br />
+<a data-sveltekit-reload href="/timeline-animated">Timeline Animated</a>
+<br /><br />
+<a data-sveltekit-reload href="/stories">Stories</a> <br /><br />
+
+<style>
+  a {
+    color: white;
+    font-size: 36px;
+    text-decoration: none;
+  }
+</style>
