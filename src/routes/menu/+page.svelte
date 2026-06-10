22c833e06618576e7f256b88705cba94919a63ec
Giacomo Nanni <gn.nanni@gmail.com>
2026-06-10 23:55:54 +0200

    chore margins

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -1,15 +1,21 @@
-<a data-sveltekit-reload href="/">Home</a> <br /><br />
-<a data-sveltekit-reload href="/latest">Latest</a> <br /><br />
-<a data-sveltekit-reload href="/timeline">Timeline</a> <br /><br />
-<a data-sveltekit-reload href="/timeline-animated">Timeline Animated</a>
-<br /><br />
-<a data-sveltekit-reload href="/timeline-categorical">Timeline Categorical</a>
-<br /><br />
-<a data-sveltekit-reload href="/map">Map</a> <br /><br />
-<a data-sveltekit-reload href="/map-pixel">Map Pixel</a> <br /><br />
-<a data-sveltekit-reload href="/stories">Stories</a> <br /><br />
+<section>
+  <a data-sveltekit-reload href="/">Home</a> <br /><br />
+  <a data-sveltekit-reload href="/latest">Latest</a> <br /><br />
+  <a data-sveltekit-reload href="/timeline">Timeline</a> <br /><br />
+  <a data-sveltekit-reload href="/timeline-animated">Timeline Animated</a>
+  <br /><br />
+  <a data-sveltekit-reload href="/timeline-categorical">Timeline Categorical</a>
+  <br /><br />
+  <!-- <a data-sveltekit-reload href="/map">Map</a> <br /><br />
+<a data-sveltekit-reload href="/map-pixel">Map Pixel</a> <br /><br /> -->
+  <a data-sveltekit-reload href="/stories">Stories</a> <br /><br />
+</section>
 
 <style>
+  section {
+    margin: 1rem;
+  
+  }
   a {
     color: white;
     font-size: 36px;
