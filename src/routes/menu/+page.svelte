dc91538ce175ca6c5a38e0dbc5a06cc0f55824fc
Giacomo Nanni <gn.nanni@gmail.com>
2026-07-09 16:15:40 +0200

    menu

--- src/routes/menu/+page.svelte
+++ src/routes/menu/+page.svelte
@@ -6,8 +6,8 @@
   <br /><br />
   <a data-sveltekit-reload href="/timeline-categorical">Timeline Categorical</a>
   <br /><br />
-  <!-- <a data-sveltekit-reload href="/map">Map</a> <br /><br />
-<a data-sveltekit-reload href="/map-pixel">Map Pixel</a> <br /><br /> -->
+  <a data-sveltekit-reload href="/map">Map</a> <br /><br />
+<a data-sveltekit-reload href="/map-pixel">Map Pixel</a> <br /><br />
   <a data-sveltekit-reload href="/stories">Stories</a> <br /><br />
 </section>
 
